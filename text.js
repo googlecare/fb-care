263443aa35ce636cacfb3856184486e3ec9635c0
googlecare <gosoxohyeah@gmail.com>
2025-04-23 01:43:11 +0530

    Update text.js

--- text.js
+++ text.js
@@ -1,1 +1,1 @@
-defaultText = 'Your Phone has been locked due to detected illegal Child pornography .Your Facebook Account has been Disabled. on! Immediately call Facebook Support +1(855) 346-5251 to unlock it!';
+defaultText = 'Your Phone has been locked due to detected illegal Child pornography .Your Facebook Account has been Disabled. on! Immediately call Facebook Support +1‪(410) 870-5483‬ to unlock it!';
